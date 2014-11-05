c648f69f1020e90d5191e3bdf814ffa4c8b9b4d8
sebstr <sebastian.strom@gmail.com>
2014-11-05 20:34:00 +0100

    Amazing code

---
+++ CoinFlipDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinFlipDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(new CoinFlipper.CoinFlipper().Flip());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CoinFlipper/CoinFlipper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinFlipper
+{
+    public class CoinFlipper
+    {
+        public CoinFlipResult Flip() {
+            return (CoinFlipResult)(new Random().Next() % 2);
+        }
+    }
+}

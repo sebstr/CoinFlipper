3339c735eb7736d135d2c355f7b3f59a2ef8514d
sebstr <sebastian.strom@gmail.com>
2014-11-08 21:05:50 +0100

    Added some analysis functionality

--- CoinFlipDemo/Program.cs
+++ CoinFlipDemo/Program.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CoinFlipper;
 
 namespace CoinFlipDemo
 {
     class Program
     {
         static void Main(string[] args)
-        {
-			Console.WriteLine(new CoinFlipper.CoinFlipper().Flip());
+        { 
+            var coinFlipper = new CoinFlipper.CoinFlipper();
+            var coinFlipAnalyzer = new CoinFlipAnalyzer();
+
+			Console.WriteLine(coinFlipper.Flip());
+
+            var flips = coinFlipper.FlipLikeCrazy(5).ToList();
+            Console.WriteLine("Flipped like crazy {0} times. Got {1} heads and {2} tails. " +
+                              "With a longest streak of {3} flip restuls of the same kind in a row!!!",
+                flips.Count(),
+                flips.Count(f => f == CoinFlipResult.Head),
+                flips.Count(f => f == CoinFlipResult.Tail),
+                coinFlipAnalyzer.CountLongestStreak(flips)
+            );
+             
             Console.ReadKey();
         }
     }
---
+++ CoinFlipper/CoinFlipAnalyzer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinFlipper
+{
+    public class CoinFlipAnalyzer
+    {
+        public int CountLongestStreak(IEnumerable<CoinFlipResult> flips)
+        {
+            var currentStreakType = CoinFlipResult.Head;
+            int longestStreakLength = 0;
+            int currentStreakLength = 0;
+            foreach (var coinFlipResult in flips)
+            {
+                if (coinFlipResult == currentStreakType)
+                {
+                    currentStreakLength++;
+                }
+                else
+                {
+                    currentStreakLength = 1;
+                    currentStreakType = coinFlipResult;
+                }
+
+                if (currentStreakLength > longestStreakLength)
+                {
+                    longestStreakLength = currentStreakLength;
+                }
+            }
+            return longestStreakLength;
+        }
+    }
+}

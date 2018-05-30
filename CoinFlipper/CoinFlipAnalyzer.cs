e413f6fb32243dfb15a7044ae8329f64d9a1a409
Seb <sebastian.strom@savelend.se>
2018-05-30 12:59:28 +0200

    remove implementation

--- CoinFlipper/CoinFlipAnalyzer.cs
+++ CoinFlipper/CoinFlipAnalyzer.cs
@@ -10,27 +10,7 @@ namespace CoinFlipper
     {
         public int CountLongestStreak(IEnumerable<CoinFlipResult> flips)
         {
-            var currentStreakType = CoinFlipResult.Head;
-            int longestStreakLength = 0;
-            int currentStreakLength = 0;
-            foreach (var coinFlipResult in flips)
-            {
-                if (coinFlipResult == currentStreakType)
-                {
-                    currentStreakLength++;
-                }
-                else
-                {
-                    currentStreakLength = 1;
-                    currentStreakType = coinFlipResult;
-                }
-
-                if (currentStreakLength > longestStreakLength)
-                {
-                    longestStreakLength = currentStreakLength;
-                }
-            }
-            return longestStreakLength;
+            return 0;
         }
     }
 }
